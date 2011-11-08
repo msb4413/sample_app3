21a71e237605b1d7c0b2ff8d4c7d22866106f1d9
Matt <shadowed666shadowed@yahoo.com>
2011-11-08 10:50:55 -0600

    about title pages

--- spec/controllers/pages_controller_spec.rb
+++ spec/controllers/pages_controller_spec.rb
@@ -23,4 +23,16 @@ describe PagesController do
     end
   end
 
+  describe "GET 'home'" do
+    it "returns http success" do
+      get 'home'
+      response.should be_success
+    end
+    
+    it "should have the right title" do
+      get 'home'
+      response.should have_selector("title", :content => "Kays App | Home")
+    end
+  end
+  
 end
