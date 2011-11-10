901838b860f6834666cd56d640e5cd70975e8168
unknown <mbuckhol@.(none)>
2011-11-10 09:20:15 -0600

    spork test

--- spec/controllers/pages_controller_spec.rb
+++ spec/controllers/pages_controller_spec.rb
@@ -7,12 +7,20 @@ describe PagesController do
       get 'home'
       response.should be_success
     end
+        it "should have the right title" do
+      get 'home'
+      response.should have_selector("title", :content => "Kays App | home")
+    end
   end
 
   describe "GET 'contact'" do
     it "returns http success" do
       get 'contact'
       response.should be_success
+    end
+        it "should have the right title" do
+      get 'contact'
+      response.should have_selector("title", :content => "Kays App | contact")
     end
   end
   
@@ -21,17 +29,9 @@ describe PagesController do
       get 'about'
       response.should be_success
     end
-  end
-
-  describe "GET 'home'" do
-    it "returns http success" do
-      get 'home'
-      response.should be_success
-    end
-    
-    it "should have the right title" do
-      get 'home'
-      response.should have_selector("title", :content => "Kays App | Home")
+        it "should have the right title" do
+      get 'about'
+      response.should have_selector("title", :content => "Kays App | about")
     end
   end
   
